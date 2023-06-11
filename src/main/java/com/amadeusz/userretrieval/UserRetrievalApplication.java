ad24da829dfe5dac9b1dc9f23ba976c3a096357d
AmadeuszPL <amadeusz.derylak@gmail.com>
2023-06-11 13:29:22 +0200

    Renamed main app class to fit naming convention.

--- src/main/java/com/amadeusz/userretrieval/UserRetrievalApplication.java
+++ src/main/java/com/amadeusz/userretrieval/UserRetrievalApplication.java
@@ -1,13 +1,13 @@
-package com.amadeusz.userretrival;
+package com.amadeusz.userretrieval;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserretrivalApplication {
+public class UserRetrievalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserretrivalApplication.class, args);
+		SpringApplication.run(UserRetrievalApplication.class, args);
 	}
 
 }
--- src/test/java/com/amadeusz/userretrieval/UserRetrievalApplicationTests.java
+++ src/test/java/com/amadeusz/userretrieval/UserRetrievalApplicationTests.java
@@ -1,10 +1,10 @@
-package com.amadeusz.userretrival;
+package com.amadeusz.userretrieval;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserretrivalApplicationTests {
+class UserRetrievalApplicationTests {
 
 	@Test
 	void contextLoads() {
