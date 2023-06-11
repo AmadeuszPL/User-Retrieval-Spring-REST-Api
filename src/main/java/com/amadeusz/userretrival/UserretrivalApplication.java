1f858adf9c1aaadb73ec4d7342acf762096222cc
AmadeuszPL <amadeusz.derylak@gmail.com>
2023-06-11 13:12:50 +0200

    Initial commit.

---
+++ src/main/java/com/amadeusz/userretrival/UserretrivalApplication.java
@@ -0,0 +1,13 @@
+package com.amadeusz.userretrival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserretrivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserretrivalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amadeusz/userretrival/UserretrivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amadeusz.userretrival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserretrivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
