1e2c684688059c4bb5e5a4e90f9cd1f39d18b27e
AmadeuszPL <amadeusz.derylak@gmail.com>
2023-06-13 00:06:25 +0200

    Moved EnableJpaRepositories annotation to UserRetrievalConfiguration.

--- src/main/java/com/amadeusz/userretrieval/UserRetrievalConfiguration.java
+++ src/main/java/com/amadeusz/userretrieval/UserRetrievalConfiguration.java
@@ -2,9 +2,11 @@ package com.amadeusz.userretrieval;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableJpaRepositories
 public class UserRetrievalConfiguration {
 
     @Bean
